61b1bff79f50d46ba5502a350f6c637f52378697
PedroToninCorebiz <pedro.tonin@corebiz.ag>
2026-02-16 17:57:26 -0300

    refactor(auth): remove forgot password screen and update layout accordingly

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,7 +5,6 @@ export default function AuthLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="register" />
-      <Stack.Screen name="forgot-password" />
     </Stack>
   );
 }
